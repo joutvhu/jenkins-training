48fae06b0a47f534627c342f9d2ed866e4a19726
Giao Ho <joutvhu@gmail.com>
2023-07-16 00:55:36 +0700

    Add demo code

---
+++ demo/src/test/java/com/joutvhu/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joutvhu.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
